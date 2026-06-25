f7033619934395d77a4257bb3cf9df4b87d9691b
Sane <sanemboniswa10@gmail.com>
2026-06-25 15:30:00 +0200

    Updated Cybersecurity Awareness Assistant - Part 3 Complete

---
+++ SaveManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using System.IO;
+
+namespace CybersecurityChatbot
+{
+    public static class SaveManager
+    {
+        public static void SaveTasks(List<TaskItem> tasks)
+        {
+            List<string> lines = new List<string>();
+
+            foreach (TaskItem task in tasks)
+            {
+                lines.Add(
+                    task.Title + "|" +
+                    task.ReminderDate.ToShortDateString() + "|" +
+                    task.Completed);
+            }
+
+            File.WriteAllLines("tasks.txt", lines);
+        }
+    }
+}
